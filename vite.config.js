73be028602905742c26adb56cca24d60940ae322
Purmarssur-Avinash <avinash.purmarssur@gmail.com>
2025-06-27 10:52:23 +0400

    Deploying to GITpages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
+  base: "Purmarssur-Avinash",
   plugins: [react()],
 })
